1baf249a7aab0c9980b30fd8261d2fec3517ab8e
im-g <guidoimbachi@unicauca.edu.co>
2022-01-26 12:50:01 -0500

    Front base

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <helloWorld msg="Welcome "/>
+    
+    <Login/>
+    -->
+    <router-view/>
+</template>
+
+<script>
+//import Login from './components/Usuario/Login.vue' 
+export default {
+  name: 'App',
+  components: {
+    //Login,
+  }
+}
+</script>
