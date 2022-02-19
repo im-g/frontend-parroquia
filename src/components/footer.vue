59e479bf85ed0a5bc752f2b0ed5450e4b50c277d
nicoled970918 <nicoled@unicauca.edu.co>
2022-02-18 21:30:45 -0500

    ajuste pantalla inicio - creación de componentes

---
+++ src/components/footer.vue
@@ -0,0 +1,90 @@
+<template>
+          <!-- Footer -->
+      <footer class="text-center text-lg-start bg-light text-muted">
+        <!-- Section: Social media -->
+        <section
+          class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+        >
+          <!-- Left -->
+          <div class="me-5 d-none d-lg-block">
+            <span>Contáctenos:</span>
+          </div>
+          <!-- Left -->
+
+          <!-- Right -->
+          <div>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-github"></i>
+            </a>
+          </div>
+          <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+              <!-- Grid column -->
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                  <i class="fas fa-gem me-3"></i>Parroquia Santísima Trinidad
+                </h6>
+                <p>
+                  Parroquia Santísima Trinidad, en el barrio Palacé de Popayán-Cauca, fundada en el año 1972.
+                </p>
+              </div>
+              <!-- Grid column -->
+
+              <!-- Grid column -->
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                  Ubicación
+                </h6>
+                <p><i class="fas fa-home me-3"></i> Cra. 6D # 26N - 67, Palacé, Popayán</p>
+                <p>
+                  <i class="fas fa-envelope me-3"></i>
+                  santisimatrinidadpop@gmail.com
+                </p>
+                <p><i class="fas fa-phone me-3"></i>Teléfono fijo: 8234681</p>
+              </div>
+              <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+          </div>
+        </section>
+        <!-- Section: Links  -->
+      </footer>
+      <!-- Footer -->
+</template>
+
+<script>
+export default
+{
+
+        data(){
+            return {
+            footerComp: ''
+            }
+        }
+
+}
+</script>
