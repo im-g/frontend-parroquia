6b26e994a98fd7e49134b6f8030c0b195299d6ef
nicoled970918 <nicoled@unicauca.edu.co>
2022-02-22 19:39:29 -0500

    sesiones pagina ppal

--- src/components/Servicios/principal.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h5 style="align-text:center">Bienvenido!! Estamos trabajando en la siguiente vista....</h5>
-</template>
-<script>
-export default {
-  name: 'principal',
-}
-</script>
